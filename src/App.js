9d41004cf68ccf7d8950d7ced1c9051c336f7b86
Shubham Patrick <ahthneeuhl@gmail.com>
2022-06-13 08:14:25 +0530

    Add routing to our project

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Projects from "./pages/Projects";
+import Posts from "./pages/Posts";
+import Post from "./components/Post";
+import Navbar from "./components/Navbar";
+
 const App = () => {
-  return null;
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/posts" element={<Posts />} />
+        <Route path="/posts/:slug" element={<Post />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
