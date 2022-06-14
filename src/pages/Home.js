ce3b26faa65529fd7cb0519facc16a6c4f66272c
Shubham Patrick <ahthneeuhl@gmail.com>
2022-06-14 20:23:00 +0530

    Work on Home Page/ Hero Section

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,13 @@
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <main>
+      <section className="bg-green-50 absolute w-full h-full">
+        <h1 className="flex justify-center min-h-screen pt-12 lg:pt-64 px-8 text-6xl text-red-900 font-bold lordish lg:leading-snug hero-name">
+          Hey there, I'm Shubham Patrick.
+        </h1>
+      </section>
+    </main>
+  );
 };
 
 export default Home;
